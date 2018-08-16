c7d066cbb8b32fd5958eebcb68e313fad18c8272
MininMobile <miningamer@gmail.com>
2018-08-16 13:13:23 +0100

    add shortcut keys

--- ntp.js
+++ ntp.js
@@ -1,6 +1,16 @@
 let search = document.getElementById("search");
 let wrapper = document.getElementById("wrapper");
+let shortcuts = document.getElementsByTagName("a");
 
 search.addEventListener("input", (e) => search.value ? wrapper.classList.add("focus") : wrapper.classList.remove("focus"));
+
+document.addEventListener("keydown", (e) => {
+	if (search.value) return;
+
+	if (!isNaN(parseInt(e.key)))
+		if (parseInt(e.key) <= 6 && parseInt(e.key) >= 1)
+			shortcuts[parseInt(e.key) - 1].click();
+});
+
 
 search.focus();
